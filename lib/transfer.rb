5d02929b1cb9b3c3bf55102ce7e912ac2a2eeeb7
Jouse then <jthen93@gmail.com>
2021-02-28 19:56:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,4 +26,11 @@ class Transfer
       return "Transaction rejected. Please check your account balance."
     end
   end
+  
+  def reverse_transfer
+    if @status == "complete" 
+      @sender.deposit(amount)
+      @receiver.balance -= @amount
+    end
+  end
 end
