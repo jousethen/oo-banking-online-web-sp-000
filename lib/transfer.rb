285d1110d238a93e96ef0a96956155316aa84a88
Jouse then <jthen93@gmail.com>
2021-02-28 19:55:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
+    if self.valid? && @status == "pending" && @sender.balance > @amount
       @sender.balance -= @amount
       @receiver.deposit(amount)
       @status = "complete"
